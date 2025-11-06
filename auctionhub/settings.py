d7238c0c3772f679bb1cf741adf54487fe9468d1
Jeffrey <jeff@smashscrap.ca>
2025-11-05 18:51:28 -0600

    a

--- auctionhub/settings.py
+++ auctionhub/settings.py
@@ -135,3 +135,12 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
